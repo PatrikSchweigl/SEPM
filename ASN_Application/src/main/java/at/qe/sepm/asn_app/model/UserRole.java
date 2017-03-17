3455b256ce7cb7c36fac6eafd08fe940471ec755
csaq6161 <stefan.mattersberger@student.uibk.ac.at>
2017-03-17 06:56:53 +0100

    renamed project

---
+++ ASN_Application/src/main/java/at/qe/sepm/asn_app/model/UserRole.java
@@ -0,0 +1,12 @@
+package at.qe.sepm.asn_app.model;
+
+/**
+ * Enumeration of available user roles.
+ */
+public enum UserRole {
+
+    ADMIN,
+    MANAGER,
+    EMPLOYEE
+
+}
---
+++ ASN_Application/src/main/java/at/qe/sepm/asn_app/ui/controllers/UserListController.java
@@ -0,0 +1,32 @@
+package at.qe.sepm.asn_app.ui.controllers;
+
+import at.qe.sepm.asn_app.services.UserService;
+import at.qe.sepm.asn_app.model.User;
+
+import java.util.Collection;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+/**
+ * Controller for the user list view.
+ *
+ * @author Michael Brunner <Michael.Brunner@uibk.ac.at>
+ */
+@Component
+@Scope("view")
+public class UserListController {
+
+    @Autowired
+    private UserService userService;
+
+    /**
+     * Returns a list of all users.
+     *
+     * @return
+     */
+    public Collection<User> getUsers() {
+        return userService.getAllUsers();
+    }
+
+}
