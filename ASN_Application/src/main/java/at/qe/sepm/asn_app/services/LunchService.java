5c96c3866f06d6c100d88b502f3eb9f5e6e891e6
Lukas <lukas.aukenthaler@student.uibk.ac.at>
2017-03-21 13:50:24 +0100

    Copied services skeletons

--- ASN_Application/src/main/java/at/qe/sepm/asn_app/services/LunchService.java
+++ ASN_Application/src/main/java/at/qe/sepm/asn_app/services/LunchService.java
@@ -1,8 +1,14 @@
 package at.qe.sepm.asn_app.services;
+import at.qe.sepm.asn_app.models.nursery.Message;
+import at.qe.sepm.asn_app.repositories.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by Stefan Mattersberger <stefan.mattersberger@student.uibk.ac.at>
  * on 20.03.2017
  */
 public class LunchService {
+    @Autowired MessageRepository messageRepository;
 }
