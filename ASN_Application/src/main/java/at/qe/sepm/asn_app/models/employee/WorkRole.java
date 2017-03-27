7008212cbead6fbccb7fe6701e58df224af7b532
csaq6161 <stefan.mattersberger@student.uibk.ac.at>
2017-03-27 14:47:29 +0200

    Enum Iterations in EnumController to provide dynamically update on changes in Enums

--- ASN_Application/src/main/java/at/qe/sepm/asn_app/models/employee/WorkRole.java
+++ ASN_Application/src/main/java/at/qe/sepm/asn_app/models/employee/WorkRole.java
@@ -4,7 +4,7 @@ package at.qe.sepm.asn_app.models.employee;
  * Created by Stefan Mattersberger <stefan.mattersberger@student.uibk.ac.at>
  * on 21.03.2017
  */
-public enum Role {
+public enum WorkRole {
     PEDAGOGUE,
     TRAINEE
 }
