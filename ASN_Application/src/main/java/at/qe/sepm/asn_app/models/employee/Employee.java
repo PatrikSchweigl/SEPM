069c58881da5ec5dea2b3e1b33a47cbee8527126
csaq6161 <stefan.mattersberger@student.uibk.ac.at>
2017-04-19 15:50:57 +0200

    child sample data

--- ASN_Application/src/main/java/at/qe/sepm/asn_app/models/employee/Employee.java
+++ ASN_Application/src/main/java/at/qe/sepm/asn_app/models/employee/Employee.java
@@ -15,7 +15,6 @@ public class Employee extends User{
 
     private static final long serialVersionUID = 1L;
 
-
     private String birthday;
     private String location;
     private String streetName;
