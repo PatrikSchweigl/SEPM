c82cb9510ef3342de2397235117bb19e7c6f908e
Bernd Menia <bernd.menia@student.uibk.ac.at>
2017-05-02 11:11:01 +0200

    - Added /models/ownExceptions/ParentConstraintException.java

---
+++ ASN_Application/src/main/java/at/qe/sepm/asn_app/models/ownExceptions/ParentConstraintException.java
@@ -0,0 +1,12 @@
+package at.qe.sepm.asn_app.models.ownExceptions;
+
+/**
+ * Created by root on 26.04.17.
+ */
+public class ParentConstraintException extends Exception {
+
+    public ParentConstraintException(String message) {
+        super(message);
+    }
+
+}
