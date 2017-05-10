532f096c4f282207d0764622b8a7bd5ef863d56c
Patrik Schweigl <patrik.schweigl@student.uibk.ac.at>
2017-05-10 17:17:22 +0200

    privatemessages work both ways

--- ASN_Application/src/main/java/at/qe/sepm/asn_app/repositories/PrivateMessageRepository.java
+++ ASN_Application/src/main/java/at/qe/sepm/asn_app/repositories/PrivateMessageRepository.java
@@ -7,13 +7,12 @@ import org.springframework.data.repository.query.Param;
 
 import at.qe.sepm.asn_app.models.nursery.PrivateMessage;
 
+public interface PrivateMessageRepository extends AbstractRepository<PrivateMessage, Long> {
 
-public interface PrivateMessageRepository extends AbstractRepository<PrivateMessage, Long>{
-	
-    @Query("SELECT p FROM PrivateMessage p WHERE :usernameS = p.usernameSender and :usernameR = p.usernameReceiver")
-    List<PrivateMessage> getPrivateMessagesBySender(@Param("usernameS") String usernameS,
-    												@Param("usernameR") String usernameR);
+	@Query("SELECT p FROM PrivateMessage p WHERE (:usernameS = p.usernameSender and :usernameR = p.usernameReceiver) or (:usernameR = p.usernameSender and :usernameS = p.usernameReceiver)")
+	List<PrivateMessage> getPrivateMessagesBySender(@Param("usernameS") String usernameS,
+			@Param("usernameR") String usernameR);
 
-    @Query("SELECT p FROM PrivateMessage p WHERE :username = p.usernameReceiver")
-    List<PrivateMessage> getPrivateMessagesByReceiver(@Param("username") String id);
+	@Query("SELECT p FROM PrivateMessage p WHERE :username = p.usernameReceiver")
+	List<PrivateMessage> getPrivateMessagesByReceiver(@Param("username") String id);
 }
