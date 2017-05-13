0cfe5e31aafae18ef59f398f839c1ef59d580d1f
csaq6229 <bernd.menia@student.uibk.ac.at>
2017-05-13 14:08:19 +0200

    - Altered PasswordConstraints so that a regex is used.

--- ASN_Application/src/main/java/at/qe/sepm/asn_app/ui/constraints/PasswordConstraints.java
+++ ASN_Application/src/main/java/at/qe/sepm/asn_app/ui/constraints/PasswordConstraints.java
@@ -1,25 +1,35 @@
 package at.qe.sepm.asn_app.ui.constraints;
 
+import at.qe.sepm.asn_app.ownExceptions.BirthdayConstraintException;
+import at.qe.sepm.asn_app.ownExceptions.PasswordConstraintException;
+
 /**
  * Created by zerus on 13.05.17.
  */
 public class PasswordConstraints {
 
-    public static void checkPasswordConstraints(String password) {
-        try {
-            if (checkPasswordLength(password) == false) {
-                throw new Exception();
-            }
-        }
-        catch(Exception e) {
-            e.printStackTrace();
+    public static void checkPasswordConstraints(String password) throws PasswordConstraintException {
+        if (checkPasswordComplexity(password) == false) {
+            throw new PasswordConstraintException("The password doesn't match the criteria.");
         }
     }
 
-    public static boolean checkPasswordLength(String password) {
-        if (password.length() < 8 || password.length() > 21) {
-            return false;
-        }
-        return true;
+
+    /**
+     * ^                 # start-of-string
+     * (?=.*[0-9])       # a digit must occur at least once
+     * (?=.*[a-z])       # a lower case letter must occur at least once
+     * (?=.*[A-Z])       # an upper case letter must occur at least once
+     * (?=.*[@#$%^&+=])  # a special character must occur at least once
+     * (?=\S+$)          # no whitespace allowed in the entire string
+     * .{8,}             # anything, at least eight places though
+     * $                 # end-of-string
+     * @param password
+     * @return
+     * @see <a href="http://stackoverflow.com/questions/3802192/regexp-java-for-password-validation">http://stackoverflow.com/questions/3802192/regexp-java-for-password-validation</a>
+     */
+    public static boolean checkPasswordComplexity(String password) {
+        String pattern = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
+        return password.matches(pattern);
     }
 }
