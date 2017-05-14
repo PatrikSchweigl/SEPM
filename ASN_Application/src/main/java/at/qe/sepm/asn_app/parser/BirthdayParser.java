ec0d1850737b6e18b09bea80cc6d21d508e3f01d
csaq6229 <bernd.menia@student.uibk.ac.at>
2017-05-14 16:32:13 +0200

    Added JavaDoc for BirthdayParser.

--- ASN_Application/src/main/java/at/qe/sepm/asn_app/parser/BirthdayParser.java
+++ ASN_Application/src/main/java/at/qe/sepm/asn_app/parser/BirthdayParser.java
@@ -8,7 +8,13 @@ import java.time.format.DateTimeFormatter;
  * Created by zerus on 09.05.17.
  */
 public class BirthdayParser {
-
+    
+    /**
+     * The birthday String gets parsed to a LocalDateTime of which then the seconds until now are calculated and
+     * returned as a <code>long</code>.
+     * @param birthday The birthday to be converted to seconds.
+     * @return The amount of seconds from birthday to now.
+     */
     public static long parseBirthdayToLong(String birthday) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
         LocalDateTime birthdayTmp = LocalDateTime.parse(birthday + " 00:00", formatter);
