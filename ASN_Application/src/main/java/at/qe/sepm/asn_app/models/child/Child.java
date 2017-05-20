59225005aa7795e711868619cf0a811b6da5499c
Bernd Menia <bernd.menia@student.uibk.ac.at>
2017-05-20 17:00:15 +0200

    - Updated class JavaDoc for Child. - Deleted out commented code. - Added equals() for Child. - Added toString() for Child.

--- ASN_Application/src/main/java/at/qe/sepm/asn_app/models/child/Child.java
+++ ASN_Application/src/main/java/at/qe/sepm/asn_app/models/child/Child.java
@@ -9,31 +9,40 @@ import org.springframework.data.domain.Persistable;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
-//import org.joda.time.DateTime;
 import java.util.Set;
 
 
 /**
  * Created by Bernd Menia <bernd.menia@student.uibk.ac.at>
  * on 17.03.17.
+ * Edit by Stefan Mattersberger on 18.03.2017
+ *
+ * Child contains all basic information for a child such as the name, birthday, gender,
+ * the parents, an emergency number, the religion, allergies and food intolerances.
+ * Additionally Child holds information about the caregivers in case a Parent is not available,
+ * the custody regarding that the parents are divorced and references to all siblings of the child.
+ * In contrary to Employee and Parent Child does not inherit from UserData.
+ * @see at.qe.sepm.asn_app.models.employee.Employee
+ * @see Caregiver
+ * @see Custody
+ * @see Gender
+ * @see Parent
+ * @see Sibling
+ * @see at.qe.sepm.asn_app.models.UserData
  */
-/** Edit by Stefan Mattersberger on 18.03.2017 */
 @Entity
-public class Child implements Persistable<Long>{
+public class Child implements Persistable<Long> {
+
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String firstName;
-
-
-
     private String lastName;
     private String birthday;
     private String imgName;
     private Gender gender;
-
 
     @ManyToOne(optional = false)
     private Parent parent1;
@@ -46,7 +55,6 @@ public class Child implements Persistable<Long>{
     private Set<String> allergies;
     @ElementCollection(targetClass = String.class)
     private Set<String> foodIntolerances;
-
 
     @OneToMany
     private Set<Sibling> siblings;
@@ -61,28 +69,6 @@ public class Child implements Persistable<Long>{
     @OneToMany
     private Set<Caregiver> caregivers;
 
-    /*@ElementCollection(targetClass=String.class)
-    private Set<String> furtherRemarks;
-    @ManyToOne(optional = false)
-    private Parent parent1;
-    @ManyToOne(optional = false)
-    private Parent parent2;
-    private String emergencyNumber;
-    @ElementCollection(targetClass=String.class)
-    private Set<String> allergies;
-    @ElementCollection(targetClass=String.class)
-    private Set<String> foodIntolerances;
-    @OneToMany
-    private Set<Sibling> listSiblings;
-    @OneToMany
-    private Set<PairTime> pairTime;
-    @Enumerated(EnumType.STRING)
-    private Custody custody;
-    @Enumerated(EnumType.STRING)
-    private Religion religion;
-    @OneToMany
-    private Set<Caregiver> caregivers;
-    */
 
     /* CONSTRUCTORS */
     public Child() {}
@@ -95,6 +81,7 @@ public class Child implements Persistable<Long>{
         this.gender = gender;
         this.parent1 = parent;
     }
+
     public Child(String firstName, String lastName, String birthday, String imgName, Gender gender, Parent par1, Parent par2, String emergencyNumber, Custody custody, Religion religion) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -107,6 +94,7 @@ public class Child implements Persistable<Long>{
         this.custody = custody;
         this.religion = religion;
     }
+
 
     public String getPrimaryParentFullName(){
         return parent1.getFirstName() + " " + parent1.getLastName();
@@ -244,5 +232,59 @@ public class Child implements Persistable<Long>{
     @Override
     public boolean isNew() {
         return (null == firstName && null == lastName);
+    }
+
+
+    /**
+     * Equals for Child doesn't check for equality of every attribute because
+     * even though some attributes like religion may be different the child
+     * could still be the "same" person. We focus only on the main attributes
+     * to determine equality.
+     * @param obj The object to be compared. If it's not an instance of Child then <code>false</code> gets returned immediately.
+     * @return <code>true</code> iff the main attributes are equal of the current child and the parameter; <code>false</code> in any other case.
+     * @see Gender
+     * @see Parent
+     * @see Sibling
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        else if (!(obj instanceof Child)) {
+            return false;
+        }
+
+        Child other = (Child) obj;
+        if (this.birthday.equals(other.birthday) &&
+                this.firstName.equals(other.firstName) &&
+                this.gender.equals(other.gender) &&
+                this.lastName.equals(other.lastName) &&
+                this.parent1.equals(other.parent1) &&
+                this.parent2.equals(other.parent2) &&
+                this.siblings.equals(other.siblings)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        return "FirstName: " + firstName + "\n" +
+                "LastName: " + lastName + "\n" +
+                "Birthday: " + birthday + "\n" +
+                "Gender: " + gender + "\n" +
+                "Religion: " + religion + "\n" +
+                "Parent1: " + parent1 + "\n" +
+                "Parent2: " + parent2 + "\n" +
+                "Custody: " + custody + "\n" +
+                "Siblings: " + siblings + "\n" +
+                "Allergies: " + allergies + "\n" +
+                "FoodIntolerances: " + foodIntolerances + "\n" +
+                "EmergencyNumber: " + emergencyNumber + "\n" +
+                "Caregivers: " + caregivers;
     }
 }
