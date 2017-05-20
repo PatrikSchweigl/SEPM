ae8a35209b6ebbc0d6ee231684e3806b7fc910ad
Bernd Menia <bernd.menia@student.uibk.ac.at>
2017-05-20 13:28:22 +0200

    Added constraints and JavaDoc for checkDateConstraints() in TaskConstraints.

--- ASN_Application/src/main/java/at/qe/sepm/asn_app/ui/constraints/TaskConstraints.java
+++ ASN_Application/src/main/java/at/qe/sepm/asn_app/ui/constraints/TaskConstraints.java
@@ -3,6 +3,8 @@ package at.qe.sepm.asn_app.ui.constraints;
 import at.qe.sepm.asn_app.models.nursery.Task;
 import at.qe.sepm.asn_app.ownExceptions.TaskConstraintException;
 
+import java.util.Date;
+
 /**
  * Created by Bernd Menia <bernd.menia@student.uibk.ac.at>
  * on 20.05.17 12:57 CEST.
@@ -32,7 +34,20 @@ public class TaskConstraints {
     }
 
 
+    /**
+     * Begin- and ending date of task must fulfill the following conditions:
+     * - The begin date must be after the current date.
+     * - The begin date must before the ending date.
+     * - They must not be equal.
+     * @param task The Task to be checked.
+     * @return false if the begin date is before the current date or if the begin date is after the ending date.
+     */
     public static boolean checkDateConstraints(Task task) {
+        if (task.getBeginDate().before(new Date()) ||
+                task.getBeginDate().after(task.getEndingDate()) ||
+                task.getBeginDate().equals(task.getEndingDate())) {
+            return false;
+        }
         return true;
     }
 }
