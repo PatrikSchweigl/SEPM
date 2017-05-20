8ab340e0b16c2f638b05e8490f0264f49d70fd7d
Bernd Menia <bernd.menia@student.uibk.ac.at>
2017-05-20 17:19:17 +0200

    Updated JavaDoc for WorkRole

--- ASN_Application/src/main/java/at/qe/sepm/asn_app/models/employee/WorkRole.java
+++ ASN_Application/src/main/java/at/qe/sepm/asn_app/models/employee/WorkRole.java
@@ -4,6 +4,8 @@ package at.qe.sepm.asn_app.models.employee;
  * Created by Stefan Mattersberger <stefan.mattersberger@student.uibk.ac.at>
  * on 21.03.2017
  *
+ * WorkRole is specifically assigned to an employee and determines whether
+ * the employee is a pedagogue or a trainee.
  * @see Employee
  * @see at.qe.sepm.asn_app.ui.controllers.EnumController
  */
