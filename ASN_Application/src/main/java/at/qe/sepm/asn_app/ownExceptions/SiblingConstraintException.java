f8bd45e3d75b3ea98ea24edd4eaebabd64bd5f0d
Bernd Menia <bernd.menia@student.uibk.ac.at>
2017-05-21 12:58:32 +0200

    Updated class JavaDoc of SiblingConstraintException.

--- ASN_Application/src/main/java/at/qe/sepm/asn_app/ownExceptions/SiblingConstraintException.java
+++ ASN_Application/src/main/java/at/qe/sepm/asn_app/ownExceptions/SiblingConstraintException.java
@@ -4,6 +4,11 @@ package at.qe.sepm.asn_app.ownExceptions;
 /**
  * Created by Bernd Menia <bernd.menia@student.uibk.ac.at>
  * on 22.04.17.
+ *
+ * This exception gets thrown if one of the following birthday constraints is violated:
+ * - A child is a sibling of itself.
+ * - A child has the same sibling twice or more.
+ * @see at.qe.sepm.asn_app.ui.constraints.ChildConstraints
  */
 public class SiblingConstraintException extends Exception {
 
