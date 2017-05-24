313e645394ccb0069d032011e76b00515d289f98
Patrik Schweigl <patrik.schweigl@student.uibk.ac.at>
2017-05-25 01:50:03 +0200

    finally fixed fileupload

--- ASN_Application/src/main/java/at/qe/sepm/asn_app/configs/CustomServletContextInitializer.java
+++ ASN_Application/src/main/java/at/qe/sepm/asn_app/configs/CustomServletContextInitializer.java
@@ -1,24 +1,53 @@
 package at.qe.sepm.asn_app.configs;
 
 
-import org.springframework.boot.web.servlet.ServletContextInitializer;
-
+import javax.faces.webapp.FacesServlet;
+import javax.servlet.DispatcherType;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
-/**
- * Spring configuration for servlet context.
- *
- * @author Michael Brunner <Michael.Brunner@uibk.ac.at>
- */
-
+import org.primefaces.webapp.filter.FileUploadFilter;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+@Configuration
 public class CustomServletContextInitializer implements ServletContextInitializer {
 
     @Override
     public void onStartup(ServletContext sc) throws ServletException {
         sc.setInitParameter("javax.faces.DEFAULT_SUFFIX", ".xhtml");
         sc.setInitParameter("javax.faces.PROJECT_STAGE", "Development");
-        //sc.setInitParameter("primefaces.THEME", "afterdark");
+    }
+
+    @Bean
+    public ServletRegistrationBean facesServletRegistraiton() {
+        ServletRegistrationBean registration = new ServletRegistrationBean(new FacesServlet(), new String[]{"*.xhtml"});
+        registration.setName("Faces Servlet");
+        registration.setLoadOnStartup(1);
+        return registration;
+    }
+
+    @Bean
+    public FilterRegistrationBean facesUploadFilterRegistration() {
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean(new FileUploadFilter(), facesServletRegistraiton());
+        registrationBean.setName("PrimeFaces FileUpload Filter");
+        registrationBean.addUrlPatterns("/*");
+        registrationBean.setDispatcherTypes(DispatcherType.FORWARD, DispatcherType.REQUEST);
+        return registrationBean;
+    }
+
+    @Bean
+    public ServletContextInitializer servletContextInitializer() {
+        return servletContext -> {
+            servletContext.setInitParameter("primefaces.UPLOADER", "commons");
+        };
     }
 
 }
+
+/*
+
+*/
