be6ba835d832d86b1dc58019947f83f53eb1ef8e
Patrik Schweigl <patrik.schweigl@student.uibk.ac.at>
2017-05-25 02:47:34 +0200

    drag the file into webapp folder

--- ASN_Application/src/main/java/at/qe/sepm/asn_app/ui/beans/FileBean.java
+++ ASN_Application/src/main/java/at/qe/sepm/asn_app/ui/beans/FileBean.java
@@ -1,17 +1,25 @@
 package at.qe.sepm.asn_app.ui.beans;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.Date;
 
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
+import org.apache.commons.io.FilenameUtils;
 import org.primefaces.event.FileUploadEvent;
 import org.primefaces.model.UploadedFile;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+
 
 import at.qe.sepm.asn_app.models.UserData;
 import at.qe.sepm.asn_app.models.nursery.AuditLog;
@@ -51,14 +59,24 @@ public class FileBean {
         this.file = file;
     }
 	
-	public void handleFileUpload(FileUploadEvent event) {
+	public void handleFileUpload(FileUploadEvent event) throws IOException {
 		UploadedFile file = event.getFile();
         AuditLog log = new AuditLog(getAuthenticatedUser().getUsername(),"PICTURE UPLOADED: " + getAuthenticatedUser().getUsername() + " [" + getAuthenticatedUser().getUserRole() + "] ", new Date());
         auditLogService.saveAuditLog(log);
        
-        
-        pictureService.savePicture(new Picture(file.getFileName(), getAuthenticatedUser(), new Date(), file.getFileName()));
+        System.out.println("Working Directory = " +
+                System.getProperty("user.dir"));
 		
+        Path folder = Paths.get("src/main/webapp/resources/pictures/gallery");
+        String filename = FilenameUtils.getBaseName(file.getFileName());
+        String extension = FilenameUtils.getExtension(file.getFileName());
+        Path newFile = Files.createTempFile(folder, filename, "." + extension);
+        pictureService.savePicture(new Picture(newFile.getFileName().toString(), getAuthenticatedUser(), new Date(), file.getFileName()));
+
+        InputStream input = file.getInputstream();
+        Files.copy(input, newFile, StandardCopyOption.REPLACE_EXISTING);
+        System.out.println("Uploaded file successfully saved in " + newFile);
+        
 	}
 	
     public UserData getAuthenticatedUser() {
