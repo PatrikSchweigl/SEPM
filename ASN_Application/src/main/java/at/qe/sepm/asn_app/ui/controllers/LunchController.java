0245d7174b50e2c95cf9567f09b9f7055eeb6843
Auki <lukas.aukenthaler@student.uibk.ac.at>
2017-05-29 13:48:35 +0200

    Lunch - basics

--- ASN_Application/src/main/java/at/qe/sepm/asn_app/ui/controllers/LunchController.java
+++ ASN_Application/src/main/java/at/qe/sepm/asn_app/ui/controllers/LunchController.java
@@ -1,20 +1,126 @@
 package at.qe.sepm.asn_app.ui.controllers;
 
+import at.qe.sepm.asn_app.models.nursery.Lunch;
 import at.qe.sepm.asn_app.repositories.AuditLogRepository;
-import at.qe.sepm.asn_app.repositories.LunchRepository;
+import at.qe.sepm.asn_app.services.LunchService;
+import at.qe.sepm.asn_app.utils.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
 
 /**
  * Created by Lukas Aukenthaler on 22.05.2017.
  */
+@Component
+@Scope("view")
 public class LunchController {
     @Autowired
-    private LunchRepository lunchRepository;
-
-    @Autowired
-    private AuditLogRepository auditLogRepository;
+    private LunchService lunchService;
 
 
+    private Lunch lunch;
+    private Lunch monday;
+    private Lunch tuesday;
+    private Lunch wednesday;
+    private Lunch thursday;
+    private Lunch friday;
 
+    public List<Lunch> getCurrentWeek(){
+        String[] dates = DateUtils.getWeekToString(0);
+        System.err.println("foglet");
+        System.out.println("n");
+        List<Lunch> ret = null;
+        for(String s : dates){
+            if(ret == null){
+                ret = lunchService.getLunchByDate(s);
+            }
+            else {
+                ret.addAll(lunchService.getLunchByDate(s));
+            }
+            System.err.println(s);
+            System.err.println("ugg");
+        }
+        return ret;
+    }
 
+    public List<Lunch> getNextWeek(){
+        String[] dates = DateUtils.getWeekToString(1);
+        List<Lunch> ret = null;
+        for(String s : dates){
+            if(ret == null){
+                ret = lunchService.getLunchByDate(s);
+            }
+            else {
+                ret.addAll(lunchService.getLunchByDate(s));
+            }
+        }
+        return ret;
+    }
+
+    public List<Lunch> findAll(){
+        return lunchService.findAll();
+    }
+    public void doSaveLunch(){
+        lunch = lunchService.saveLunch(lunch);
+        //mailService.sendEmail("Patrik.Schweigl@student.uibk.ac.at", "Test", "Hallo,  ich bin es, das System!");
+        lunch = null;
+        initNewLunch();
+    }
+
+    @PostConstruct
+    private void initNewLunch(){
+        lunch = new Lunch();
+    }
+
+    //getters, setters -------------------------------------------------------------------------------------------
+    public Lunch getLunch() {
+        return lunch;
+    }
+
+    public void setLunch(Lunch lunch) {
+        this.lunch = lunch;
+    }
+
+    public Lunch getMonday() {
+        return monday;
+    }
+
+    public void setMonday(Lunch monday) {
+        this.monday = monday;
+    }
+
+    public Lunch getTuesday() {
+        return tuesday;
+    }
+
+    public void setTuesday(Lunch tuesday) {
+        this.tuesday = tuesday;
+    }
+
+    public Lunch getWednesday() {
+        return wednesday;
+    }
+
+    public void setWednesday(Lunch wednesday) {
+        this.wednesday = wednesday;
+    }
+
+    public Lunch getThursday() {
+        return thursday;
+    }
+
+    public void setThursday(Lunch thursday) {
+        this.thursday = thursday;
+    }
+
+    public Lunch getFriday() {
+        return friday;
+    }
+
+    public void setFriday(Lunch friday) {
+        this.friday = friday;
+    }
 }
--- ASN_Application/src/main/java/at/qe/sepm/asn_app/utils/DateUtils.java
+++ ASN_Application/src/main/java/at/qe/sepm/asn_app/utils/DateUtils.java
@@ -12,12 +12,50 @@ public class DateUtils {
      */
     public static LocalDate[] getNextWeek(){
         LocalDate now = LocalDate.now();
-        //TODO remove holidays
+
         int x = now.getDayOfWeek().getValue();
         x = 8 - x;
         LocalDate[] ret = new LocalDate[5];
         for(int i = 0; i < 5; i++){
+            //TODO remove holidays
             ret[i] = now.plusDays(x + i);
+        }
+
+        return ret;
+    }
+    /** getCurrentWeek
+     *
+     * @return array containing LocalDates for the current week (mon-fri)
+     */
+    public static LocalDate[] getCurrentWeek(){
+        LocalDate now = LocalDate.now();
+
+        int x = now.getDayOfWeek().getValue();
+        x = 1 - x;
+        LocalDate[] ret = new LocalDate[5];
+        for(int i = 0; i < 5; i++){
+            //TODO remove holidays
+            ret[i] = now.plusDays(x + i);
+        }
+
+        return ret;
+    }
+
+    /** getWeek
+     * @param j
+     *  determines the week you want to retrieve, 0 is the current, 1 is the next one, etc.
+     * @return array containing string represantations of LocalDates for the selected week (mon-fri)
+     */
+    public static String[] getWeekToString(int j){
+        LocalDate now = LocalDate.now();
+
+        int x = now.getDayOfWeek().getValue();
+        x = 1 + j*7 - x;
+        String[] ret = new String[5];
+        for(int i = 0; i < 5; i++){
+            //TODO remove holidays
+            ret[i] = now.plusDays(x + i).toString();
+            System.err.println(ret[i]);
         }
 
         return ret;
