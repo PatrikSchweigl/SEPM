a1e2380ca973b3bcc48afb20e9438bd7e4f966bd
Patrik Schweigl <patrik.schweigl@student.uibk.ac.at>
2017-06-03 23:08:16 +0200

    bugfixing

--- ASN_Application/src/main/java/at/qe/sepm/asn_app/ui/controllers/NurseryInformationController.java
+++ ASN_Application/src/main/java/at/qe/sepm/asn_app/ui/controllers/NurseryInformationController.java
@@ -12,67 +12,69 @@ import java.util.Collection;
 import java.util.Date;
 
 /**
- * Created by Stefan Mattersberger <stefan.mattersberger@student.uibk.ac.at>
- * on 15.05.2017
+ * Created by Stefan Mattersberger <stefan.mattersberger@student.uibk.ac.at> on
+ * 15.05.2017
  */
 @Component
 @Scope("view")
 public class NurseryInformationController {
 
-    @Autowired
-    private NurseryInformationService nurseryInformationService;
-    private NurseryInformation nurseryInformation;
-    @Autowired
-    private NurseryConstraints nurseryConstraints;
+	@Autowired
+	private NurseryInformationService nurseryInformationService;
+	private NurseryInformation nurseryInformation;
+	@Autowired
+	private NurseryConstraints nurseryConstraints;
 
-    private Collection<NurseryInformation> nurseryInformations;
+	private Collection<NurseryInformation> nurseryInformations;
 
-    public NurseryInformation getNurseryInformation() {
-        return nurseryInformation;
-    }
+	public NurseryInformation getNurseryInformation() {
+		return nurseryInformation;
+	}
 
-    public void setNurseryInformation(NurseryInformation nurseryInformation) {
-        this.nurseryInformation = nurseryInformation;
-        doReloadNurseryInformation();
-    }
+	public void setNurseryInformation(NurseryInformation nurseryInformation) {
+		this.nurseryInformation = nurseryInformation;
+		doReloadNurseryInformation();
+	}
 
-    public Collection<NurseryInformation> getNurseryInformations() {
-        return nurseryInformations;
-    }
-    
-    public NurseryInformation getNurseryInformationByOriginDate(Date date){
-    	return nurseryInformationService.nurseryInformationByOriginDate(date);
-    }
-    public void setNurseryInformations(Collection<NurseryInformation> nurseryInformations) {
-        this.nurseryInformations = nurseryInformations;
-    }
+	public Collection<NurseryInformation> getNurseryInformations() {
+		return nurseryInformations;
+	}
 
+	public NurseryInformation getNurseryInformationByOriginDate(Date date) {
+		return nurseryInformationService.nurseryInformationByOriginDate(date);
+	}
 
-    @PostConstruct
-    private void initNewNurseryInformation(){
-        nurseryInformation = new NurseryInformation();
-    }
+	public void setNurseryInformations(Collection<NurseryInformation> nurseryInformations) {
+		this.nurseryInformations = nurseryInformations;
+	}
 
-    @PostConstruct
-    private void initList(){
-        setNurseryInformations(nurseryInformationService.getAllInformation());
-    }
+	@PostConstruct
+	private void initNewNurseryInformation() {
+		nurseryInformation = new NurseryInformation();
+	}
 
-    public void doSaveNurseryInformation(){
-      if(nurseryConstraints.nurseryInfoExists(nurseryInformation) || nurseryInformation.getOriginDate().compareTo(new Date()) <= 0){
-            return;
-        }
-        nurseryInformation = nurseryInformationService.saveNurseryInformation(nurseryInformation);
-        nurseryInformation = null;
-        initNewNurseryInformation();
-    }
+	@PostConstruct
+	private void initList() {
+		setNurseryInformations(nurseryInformationService.getAllInformation());
+	}
 
-    public void doDeleteNurseryInformation(){
-        nurseryInformationService.deleteNurseryInformation(nurseryInformation);
-        nurseryInformation = null;
-    }
+	public void doSaveNurseryInformation() {
+		if (nurseryConstraints.nurseryInfoExists(nurseryInformation)
+				|| nurseryInformation.getOriginDate().compareTo(new Date()) <= 0
+				|| nurseryConstraints.checkTimeConstraints(nurseryInformation) == false) {
+			return;
+		}
+		nurseryInformation = nurseryInformationService.saveNurseryInformation(nurseryInformation);
+		nurseryInformation = null;
+		initNewNurseryInformation();
+	}
 
-    public void doReloadNurseryInformation(){
-        nurseryInformation = nurseryInformationService.loadNurseryInformation(nurseryInformation.getId());
-    }
+	public void doDeleteNurseryInformation() {
+		nurseryInformationService.deleteNurseryInformation(nurseryInformation);
+		nurseryInformation = null;
+	}
+
+	public void doReloadNurseryInformation() {
+		nurseryInformation = nurseryInformationService.loadNurseryInformation(nurseryInformation.getId());
+	}
 }
