cf9ab1d921d39a219bb62451f693a476c876f460
Auki <lukas.aukenthaler@student.uibk.ac.at>
2017-06-14 15:01:08 +0200

    changed default login page for parent to calendar

--- ASN_Application/src/main/java/at/qe/sepm/asn_app/configs/LoginHandler.java
+++ ASN_Application/src/main/java/at/qe/sepm/asn_app/configs/LoginHandler.java
@@ -70,7 +70,7 @@ public class LoginHandler implements AuthenticationSuccessHandler {
         if (isAdmin) {
             return "/admin/mainpage_admin.xhtml";
         } else if (isParent) {
-            return "/parent/mainpage_active_parent.xhtml";
+            return "/parent/calendar_parent.xhtml";
         } else if (isEmployee) {
             return "/employee/mainpage_employee.xhtml";
         }else {
