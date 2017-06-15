59bf5b3f005332ea35dea6a0914a75e2c0c98d36
Bernd Menia <bernd.menia@student.uibk.ac.at>
2017-06-15 12:50:44 +0200

    LunchConstraintsTest: Updated code and added test for checkIfLunchExists().

--- ASN_Application/src/test/java/at/qe/sepm/asn_app/tests/constraints/LunchConstraintsTest.java
+++ ASN_Application/src/test/java/at/qe/sepm/asn_app/tests/constraints/LunchConstraintsTest.java
@@ -11,7 +11,9 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Scope;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.stereotype.Component;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.Calendar;
@@ -19,8 +21,7 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.TimeZone;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 /**
  * Created by Bernd Menia <bernd.menia@student.uibk.ac.at>
@@ -34,7 +35,11 @@ public class LunchConstraintsTest {
 
     @Autowired
     private LunchConstraints lunchConstraints;
-    private Lunch lunch;
+    @Autowired
+    private LunchService lunchService;
+    private Lunch lunch1;
+    private Lunch lunch2;
+    private Lunch lunch3;
 
     private Calendar calendar;
 
@@ -45,7 +50,9 @@ public class LunchConstraintsTest {
 
         calendar = GregorianCalendar.getInstance(TimeZone.getTimeZone("Europe/Vienna"));
         calendar.clear();
-        lunch = new Lunch();
+        lunch1 = new Lunch();
+        lunch2 = new Lunch();
+        lunch3 = new Lunch();
     }
 
 
@@ -53,12 +60,12 @@ public class LunchConstraintsTest {
      * Test a lunch without any violation.
      */
     @Test
-    public void test1() {
+    public void checkLunchConstraints() {
         calendar.set(2017, 5, 19, 13, 45);
-        lunch.setCost(5);
-        lunch.setDate(calendar.getTime());
-        lunch.setMeal("SpaghettiOs");
-        assertTrue(lunchConstraints.checkLunchConstraints(lunch));
+        lunch1.setCost(5);
+        lunch1.setDate(calendar.getTime());
+        lunch1.setMeal("SpaghettiOs");
+        assertTrue(lunchConstraints.checkLunchConstraints(lunch1));
     }
 
 
@@ -72,10 +79,59 @@ public class LunchConstraintsTest {
         calendar.set(2017, 5, 13, 12, 0);
         Date date = calendar.getTime();
 
-        lunch.setCost(5);
-        lunch.setDate(date);
-        lunch.setMeal("SpaghettiOs");
-        assertFalse(lunchConstraints.checkTimeConstraints(lunch));
+        lunch1.setCost(5);
+        lunch1.setDate(date);
+        lunch1.setMeal("SpaghettiOs");
+        assertFalse(lunchConstraints.checkTimeConstraints(lunch1));
+    }
+
+
+    /**
+     * Save two lunches in the database and check if it is possible to save a lunch between those days.
+     */
+    @DirtiesContext
+    @Test
+    @WithMockUser(username = "admin", authorities = {"ADMIN"})
+    public void testCheckIfLunchExists1() {
+        Calendar calendar = GregorianCalendar.getInstance(TimeZone.getTimeZone("Europe/Vienna"));
+        calendar.clear();
+        calendar.set(2017, 6, 4, 12, 0);
+        lunch1.setCost(5);
+        lunch1.setDate(calendar.getTime());
+        lunch1.setMeal("SpaghettiOs");
+        assertFalse(lunchConstraints.checkIfLunchExists(lunch1));
+
+        // Create a lunch one day before the initial lunch.
+        calendar.clear();
+        calendar.set(2017, 6, 3, 23, 59);
+        lunch2.setCost(lunch1.getCost());
+        lunch2.setDate(calendar.getTime());
+        lunch2.setMeal(lunch1.getMeal());
+        lunch2 = lunchService.saveLunch(lunch2);
+
+        // Create another lunch one day after the initial lunch.
+        calendar.clear();
+        calendar.set(2017, 6, 5, 0, 0);
+        lunch3.setCost(lunch1.getCost());
+        lunch3.setDate(calendar.getTime());
+        lunch3.setMeal(lunch1.getMeal());
+        lunch3 = lunchService.saveLunch(lunch3);
+
+        // It must still be possible to save a lunch inbetween two other lunches.
+        assertFalse(lunchConstraints.checkIfLunchExists(lunch1));
+
+        lunch1 = lunchService.saveLunch(lunch1);
+        assertNotNull(lunchService.loadLunch(lunch1.getId()));
+
+        // Delete all lunches again for completenesss.
+        lunchService.deleteLunch(lunch1);
+        assertNull(lunchService.loadLunch(lunch1.getId()));
+
+        lunchService.deleteLunch(lunch2);
+        assertNull(lunchService.loadLunch(lunch2.getId()));
+
+        lunchService.deleteLunch(lunch3);
+        assertNull(lunchService.loadLunch(lunch3.getId()));
     }
 
 
@@ -99,10 +155,8 @@ public class LunchConstraintsTest {
 
     @After
     public void cleanUp() {
-        lunch = null;
-        //lunch2 = null;
-        //lunch3 = null;
-        //lunch4 = null;
-        //lunch5 = null;
+        lunch1 = null;
+        lunch2 = null;
+        lunch3 = null;
     }
 }
