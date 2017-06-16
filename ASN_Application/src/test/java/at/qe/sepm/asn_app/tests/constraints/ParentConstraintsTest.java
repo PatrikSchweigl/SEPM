9905afdd1cdb5f5d7dfe67c9037a0f63562f4092
Bernd Menia <bernd.menia@student.uibk.ac.at>
2017-06-16 11:27:54 +0200

    ParentConstraintsTest: Commented out a lot of unnecessary code and created the main parent object with setters instead of a full constructor.

--- ASN_Application/src/test/java/at/qe/sepm/asn_app/tests/constraints/ParentConstraintsTest.java
+++ ASN_Application/src/test/java/at/qe/sepm/asn_app/tests/constraints/ParentConstraintsTest.java
@@ -5,6 +5,7 @@ import at.qe.sepm.asn_app.models.UserRole;
 import at.qe.sepm.asn_app.models.child.Child;
 import at.qe.sepm.asn_app.models.child.Sibling;
 import at.qe.sepm.asn_app.models.general.FamilyStatus;
+import at.qe.sepm.asn_app.models.general.Religion;
 import at.qe.sepm.asn_app.models.nursery.Task;
 import at.qe.sepm.asn_app.models.referencePerson.Parent;
 import at.qe.sepm.asn_app.ownExceptions.BirthdayConstraintException;
@@ -39,25 +40,20 @@ import static org.junit.Assert.assertTrue;
 @Scope("test")
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
-//@SpringApplicationConfiguration(classes = ParentRepository.class)
-//@SpringApplicationConfiguration(classes = ParentService.class)
 public class ParentConstraintsTest {
 
-    private Child child1;
-    private Child child2;
-    private Child child3;
-    private Child child4;
-    private Child child5;
-    private ArrayList<Child> listChildren;
-    private Sibling sibling1;
-    private Sibling sibling2;
-    private Sibling sibling3;
-    private ArrayList<Sibling> listSiblings;
-    private Parent parent1;
-    private Parent parent2;
-    private Parent parent3;
-    private Parent parent4;
-    private ArrayList<Parent> listParents;
+    private Child child;
+    //private Child child2;
+    //private Child child3;
+    //private Child child4;
+    //private Child child5;
+    //private ArrayList<Child> listChildren;
+    //private Sibling sibling1;
+    //private Sibling sibling2;
+    //private Sibling sibling3;
+    //private ArrayList<Sibling> listSiblings;
+    private Parent parent;
+    //private ArrayList<Parent> listParents;
     //private ParentConstraints parentConstraints;
     //private ParentService parentService;
 
@@ -73,15 +69,35 @@ public class ParentConstraintsTest {
      */
     @Before
     public void initialize() {
-        Set<Child> parentListChildren1 = new HashSet<>();
-        Set<Child> parentListChildren2 = new HashSet<>();
-        Set<Child> parentListChildren3 = new HashSet<>();
-        Set<Task> parentListTasks1 = new HashSet<>();
-        Set<Task> parentListTasks2 = new HashSet<>();
-        Set<Task> parentListTasks3 = new HashSet<>();
+        Set<Child> children = new HashSet<>();
+        //Set<Child> parentListChildren2 = new HashSet<>();
+        //Set<Child> parentListChildren3 = new HashSet<>();
+        Set<Task> tasks = new HashSet<>();
+        //Set<Task> parentListTasks2 = new HashSet<>();
+        //Set<Task> parentListTasks3 = new HashSet<>();
+
+        parent = new Parent();
+        parent.setUsername("");
+        parent.setPassword("passwd");
+        parent.setFirstName("ParentFirstName1");
+        parent.setLastName("ParentLastName1");
+        parent.setLocation("ParentLocation1");
+        parent.setStreetName("ParentStreetName1");
+        parent.setPostcode("6020");
+        parent.setBirthday("24/05/1980");
+        parent.setEmail("ParentEmail1@google.com");
+        parent.setImgName("ParentImgName1");
+        parent.setUserRole(UserRole.PARENT);
+        parent.setReligion(Religion.ISLAM);
+        parent.setPhoneNumber("4839014056");
+        parent.setNotification(true);
+        parent.setChildren(children);
+        parent.setTasks(tasks);
+        parent.setFamilyStatus(FamilyStatus.VERHEIRATET);
+        parent.setStatus(true);
 
         // Having a '0' in front of the month could maybe be a problem because usually a 0 in front of a number means oct-numbers
-        listParents = new ArrayList<>();
+        //listParents = new ArrayList<>();
         /*
         String username, String password, String firstName,
                   String lastName, String location, String streetName,
@@ -89,37 +105,32 @@ public class ParentConstraintsTest {
                   UserRole userRole, String imgName, Set<Child> children,
                   Set<Task> tasks, FamilyStatus familyStatus, boolean status
          */
-        listParents.add(parent1 = new Parent("4", "passwd", "ParentFirstName1", "ParentLastName1", "ParentLocation1",
-                                                "ParentStreetName1", "ParentPostcode1", "24/05/1980","ParentEmail1@google.com",
-                                                UserRole.PARENT, "ParentImageName1", parentListChildren1, parentListTasks1,
-                FamilyStatus.VERHEIRATET, true));
+        //listParents.add(parent1 = new Parent("4", "passwd", "ParentFirstName1", "ParentLastName1", "ParentLocation1",
+                                                //"ParentStreetName1", "ParentPostcode1", "24/05/1980","ParentEmail1@google.com",
+                                                //UserRole.PARENT, "ParentImageName1", parentListChildren1, parentListTasks1,
+                //FamilyStatus.VERHEIRATET, true));
         //listParents.add(parent1 = new Parent("", "ParentUserName1", "ParentFirstName1", "ParentLastName1", "ParentLocation1", "ParentStreetName1", "ParentPostcode1", UserRole.PARENT, "ParentImgName1", parentListChildren1, parentListTasks1, FamilyStatus.VERHEIRATET, true, "24/05/1980"));
         //listParents.add(parent2 = new Parent("", "ParentUserName2", "ParentFirstName2", "ParentLastName2", "ParentLocation2", "ParentStreetName2", "ParentPostcode2", UserRole.PARENT, "ParentImgName2", parentListChildren2, parentListTasks2, FamilyStatus.GESCHIEDEN, true, "11/11/2003"));  // Too young
         //listParents.add(parent3 = new Parent("", "ParentUserName3", "ParentFirstName3", "ParentLastName3", "ParentLocation3", "ParentStreetName3", "ParentPostcode3", UserRole.PARENT, "ParentImgName3", parentListChildren3, parentListTasks3, FamilyStatus.LEDIG, true, "30/04/1918"));   // Too old
         //listParents.add(parent4 = new Parent("", "ParentUserName3", "ParentFirstName3", "ParentLastName3", "ParentLocation3", "ParentStreetName3", "ParentPostcode3", UserRole.PARENT, "ParentImgName3", parentListChildren3, parentListTasks3, FamilyStatus.LEDIG, true, "30/04/1918"));   // Same as parent3
         //parentService = new ParentService();
 
+        /*
         listChildren = new ArrayList<>();
         listChildren.add(child1 = new Child("FirstName1", "LastName1", "03/05/2015", "ImageName1", Gender.MAENNLICH, parent1));
         listChildren.add(child2 = new Child("FirstName2", "LastName2", "04/04/2014", "ImageName2", Gender.WEIBLICH, parent1));     // Too old
         listChildren.add(child3 = new Child("FirstName1", "LastName1", "03/05/2015", "ImageName3", Gender.MAENNLICH, parent2));
         listChildren.add(child4 = new Child("FirstName4", "LastName4", "21/02/2017", "ImageName4", Gender.WEIBLICH, parent2));    // Too young
-        listChildren.add(child5 = new Child("FirstName5", "LastName5", "03/05/2015", "ImageName5", Gender.MAENNLICH, parent3));
-
-        listSiblings = new ArrayList<>();
-        listSiblings.add(sibling1 = new Sibling(child1.getFirstName(), child1.getLastName(), child1.getBirthday()));
-        listSiblings.add(sibling2 = new Sibling(child2.getFirstName(), child2.getLastName(), child2.getBirthday()));
-        listSiblings.add(sibling3 = new Sibling(child3.getFirstName(), child3.getLastName(), child3.getBirthday()));
+        listChildren.add(child5 = new Child("FirstName5", "LastName5", "03/05/2015", "ImageName5", Gender.MAENNLICH, parent3));*/
     }
 
 
     /**
      * No constraint violation
      */
-    @Ignore
     @Test
     public void checkBirthdayConstraintsTest1() throws BirthdayConstraintException {
-        ParentConstraints.checkBirthdayConstraints(parent1);
+        ParentConstraints.checkBirthdayConstraints(parent);
     }
 
 
@@ -128,10 +139,9 @@ public class ParentConstraintsTest {
      *
      * @throws BirthdayConstraintException
      */
-    @Ignore
     @Test
     public void checkBirthdayConstraintsTest2() throws BirthdayConstraintException {
-        assertFalse(ParentConstraints.checkBirthdayConstraints(parent2));
+        assertFalse(ParentConstraints.checkBirthdayConstraints(parent));
     }
 
 
@@ -140,23 +150,20 @@ public class ParentConstraintsTest {
      *
      * @throws BirthdayConstraintException
      */
-    @Ignore
     @Test
     public void checkBirthdayConstraintsTest3() throws BirthdayConstraintException {
-        assertFalse(ParentConstraints.checkBirthdayConstraints(parent3));
+        assertFalse(ParentConstraints.checkBirthdayConstraints(parent));
     }
 
 
-    // TODO parentService throws null pointer exceptions because a new AuditLog gets created.
-    // TODO --> the userRepository is null
     @Test
     public void alreadyExistsTest() {
         //parentService.saveParent(parent1);
         //parent3 = parentRepository.save(parent3);
         //parentRepository.save(parent3);
-        parentRepository.save(parent1);
+        //parentRepository.save(parent);
         //assertTrue(ParentConstraints.alreadyExists(parent1));
-        assertTrue(parentConstraints.alreadyExists(parent1));
+        assertTrue(parentConstraints.alreadyExists(parent));
     }
 
 
@@ -165,6 +172,7 @@ public class ParentConstraintsTest {
      */
     @After
     public void cleanUp() {
+        /*
         for (Child c : listChildren) {
             c = null;
         }
@@ -179,9 +187,9 @@ public class ParentConstraintsTest {
             p = null;
         }
         listParents = null;
+        */
 
         //parentConstraints = null;
         //parentService = null;
     }
-
 }
