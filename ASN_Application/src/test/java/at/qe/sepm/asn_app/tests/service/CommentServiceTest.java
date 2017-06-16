fb663ecd1e181fab33768dd4d81ef52072f177bd
Bernd Menia <bernd.menia@student.uibk.ac.at>
2017-06-16 14:16:35 +0200

    CommentServiceTest: Added testSetterGetter() to make sure that all methods get tested (also prints out toString(); Removed unused imports and added JavaDoc to initialize() and testSingleComment().

--- ASN_Application/src/test/java/at/qe/sepm/asn_app/tests/service/CommentServiceTest.java
+++ ASN_Application/src/test/java/at/qe/sepm/asn_app/tests/service/CommentServiceTest.java
@@ -2,13 +2,10 @@ package at.qe.sepm.asn_app.tests.service;
 
 import at.qe.sepm.asn_app.models.general.Comment;
 import at.qe.sepm.asn_app.services.CommentService;
-import at.qe.sepm.asn_app.tests.controller.ContextMocker;
-import at.qe.sepm.asn_app.ui.controllers.CommentController;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.primefaces.context.RequestContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Scope;
@@ -17,12 +14,11 @@ import org.springframework.stereotype.Component;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import javax.faces.context.FacesContext;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 /**
  * Created by Bernd Menia <bernd.menia@student.uibk.ac.at>
@@ -33,11 +29,15 @@ import static org.junit.Assert.assertTrue;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 public class CommentServiceTest {
+
     @Autowired
     private CommentService commentService;
     private Comment comment;
 
 
+    /**
+     * Initialize a comment with some default values.
+     */
     @Before
     public void initialize() {
         comment = new Comment();
@@ -48,6 +48,10 @@ public class CommentServiceTest {
     }
 
 
+    /**
+     * Save a comment in the database and check if values have changed.
+     * This test only succeeds if the comment stays the same.
+     */
     @DirtiesContext
     @Test
     @WithMockUser(username = "admin", authorities = {"ADMIN"})
@@ -69,6 +73,39 @@ public class CommentServiceTest {
 
     public void testMultipleComments() {
 
+    }
+
+
+    /**
+     * This test is just for completeness to make sure
+     * that everything works 100%.
+     */
+    @Test
+    public void testSetterGetter() {
+        Calendar calendar = GregorianCalendar.getInstance();
+        calendar.clear();
+        calendar.set(2017, Calendar.JULY, 11, 16, 47);
+
+        // Initialize attributes
+        String content = "Content";
+        Date date = calendar.getTime();
+        String pictureName = "PictureName";
+        String username = "UserName";
+
+        // Set attributes
+        comment.setComment(content);
+        comment.setDate(date);
+        comment.setPictureName(pictureName);
+        comment.setUsername(username);
+
+        // Compare all attributes with getters.
+        assertEquals(content, comment.getComment());
+        assertEquals(date, comment.getDate());
+        assertEquals(pictureName, comment.getPictureName());
+        assertEquals(username, comment.getUsername());
+
+        // Print all information with toString();
+        System.out.println(comment.toString());
     }
 
 
