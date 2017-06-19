44bcc9f4ac186f1be71678d791eb17bf4b007b9f
Patrik Schweigl <patrik.schweigl@student.uibk.ac.at>
2017-06-20 01:16:09 +0200

    added reminder for monthly report

--- ASN_Application/src/main/java/at/qe/sepm/asn_app/configs/NotificationConfig.java
+++ ASN_Application/src/main/java/at/qe/sepm/asn_app/configs/NotificationConfig.java
@@ -1,12 +1,17 @@
 package at.qe.sepm.asn_app.configs;
 
 import at.qe.sepm.asn_app.models.UserData;
+import at.qe.sepm.asn_app.models.child.Child;
+import at.qe.sepm.asn_app.models.nursery.Lunch;
 import at.qe.sepm.asn_app.models.nursery.Task;
 import at.qe.sepm.asn_app.models.referencePerson.Parent;
+import at.qe.sepm.asn_app.services.LunchService;
 import at.qe.sepm.asn_app.services.MailService;
 import at.qe.sepm.asn_app.services.ParentService;
 import at.qe.sepm.asn_app.services.TaskService;
 import at.qe.sepm.asn_app.services.UserService;
+import at.qe.sepm.asn_app.ui.controllers.ReportController;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -16,70 +21,96 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 
 /**
- * Created by Stefan Mattersberger <stefan.mattersberger@student.uibk.ac.at>
- * on 05.06.2017
+ * Created by Stefan Mattersberger <stefan.mattersberger@student.uibk.ac.at> on
+ * 05.06.2017
  */
 @Configuration
 @EnableScheduling
 public class NotificationConfig {
 
-    @Autowired
-    private MailService mailService;
-    @Autowired
-    private UserService userService;
-    @Autowired
-    private ParentService parentService;
+	@Autowired
+	private MailService mailService;
+	@Autowired
+	private UserService userService;
+	@Autowired
+	private ParentService parentService;
+	@Autowired
+	private TaskService taskService;
+	@Autowired
+	private ReportController reportController;
 
-    @Autowired
-    private TaskService taskService;
+	@Scheduled(cron = "0 0 10 * * *") // 60000 for one minute - "0 0 10 * * *"
+										// 10 AM everyday
+	public void taskReminder() {
+		Collection<UserData> list = userService.getParentsByNotification();
+		String footer = "Das Kinderkrippen-Team bedankt sich für Ihre Mitarbeit!";
 
-    @Scheduled(cron="0 0 10 * * *")    //60000 for one minute - "0 0 10 * * *" 10 AM everyday
-    public void taskReminder() {
-        Collection<UserData> list = userService.getParentsByNotification();
-        String footer = "Das Kinderkrippen-Team bedankt sich für Ihre Mitarbeit!";
+		Date today = new Date();
+		Date dateToCompare = new Date();
 
-        Date today = new Date();
-        Date dateToCompare = new Date();
+		Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("Europe/Vienna"));
+		cal.setTime(today);
 
-        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("Europe/Vienna"));
-        cal.setTime(today);
+		int hrs = cal.get(Calendar.HOUR_OF_DAY);
+		int min = cal.get(Calendar.MINUTE);
+		int sec = cal.get(Calendar.SECOND);
+		int milli = cal.get(Calendar.MILLISECOND);
 
-        int hrs = cal.get(Calendar.HOUR_OF_DAY);
-        int min = cal.get(Calendar.MINUTE);
-        int sec = cal.get(Calendar.SECOND);
-        int milli = cal.get(Calendar.MILLISECOND);
+		today.setTime(today.getTime() - (min * 60 * 1000) - (hrs * 60 * 60 * 1000) - (sec * 1000) - milli);
 
-        today.setTime(today.getTime()-(min*60*1000)-(hrs*60*60*1000)-(sec*1000)-milli);
+		if (!list.isEmpty()) {
+			Iterator<UserData> itL = list.iterator();
+			while (itL.hasNext()) {
+				Parent p = parentService.loadParent(itL.next().getUsername());
+				String taskMsg = "";
+				boolean send = false;
+				Collection<Task> tasks = taskService.getAllTasksByReceiver(p.getUsername());
 
-        if(!list.isEmpty()){
-            Iterator<UserData> itL = list.iterator();
-            while(itL.hasNext()){
-                Parent p = parentService.loadParent(itL.next().getUsername());
-                String taskMsg = "";
-                boolean send = false;
-                Collection<Task> tasks = taskService.getAllTasksByReceiver(p.getUsername());
+				Iterator<Task> itT = tasks.iterator();
+				while (itT.hasNext()) {
+					Task t = itT.next();
+					cal.setTime(t.getBeginDate());
+					hrs = cal.get(Calendar.HOUR_OF_DAY);
+					min = cal.get(Calendar.MINUTE);
 
-                Iterator<Task> itT = tasks.iterator();
-                while(itT.hasNext()){
-                    Task t = itT.next();
-                    cal.setTime(t.getBeginDate());
-                    hrs = cal.get(Calendar.HOUR_OF_DAY);
-                    min = cal.get(Calendar.MINUTE);
+					dateToCompare.setTime(t.getBeginDate().getTime() - (min * 60 * 1000) - (hrs * 60 * 60 * 1000));
 
-                    dateToCompare.setTime(t.getBeginDate().getTime()-(min*60*1000)-(hrs*60*60*1000));
+					if ((dateToCompare.getTime() - today.getTime()) == 345600000) { // 4
+																					// days
+																					// before
+																					// the
+																					// task
+						send = true;
+						taskMsg += t.getDescription() + "\t|\t" + t.getFormattedDate(t.getBeginDate()) + "\t|\t"
+								+ t.getFormattedDate(t.getEndingDate()) + "\n";
+					}
+				}
+				if (send) {
+					mailService.sendEmail(p.getEmail(), "Erinnerung - Anstehende Aufgaben",
+							"Guten Tag " + p.getFirstName() + " " + p.getLastName()
+									+ "!\n\nSie haben noch 4 Tage zeit, um folgende Aufgabe(n) zu erledigen:\n\n"
+									+ taskMsg + "\n\n" + footer);
+				}
 
-                    if((dateToCompare.getTime()-today.getTime()) == 345600000){ //4 days before the task
-                        send = true;
-                        taskMsg += t.getDescription() + "\t|\t"+ t.getFormattedDate(t.getBeginDate())+"\t|\t"+ t.getFormattedDate(t.getEndingDate())+"\n";
-                    }
-                }
-                if(send){
-                    mailService.sendEmail(p.getEmail(), "Erinnerung - Anstehende Aufgaben", "Guten Tag "+p.getFirstName() + " " + p.getLastName()
-                            +"!\n\nSie haben noch 4 Tage zeit, um folgende Aufgabe(n) zu erledigen:\n\n" +taskMsg +"\n\n" +footer);
-                }
+			}
+		}
+	}
 
-
-            }
-        }
-    }
+	/*public void monthlyLunchCalc() {
+		String footer = "Das Kinderkrippen-Team bedankt sich für Ihre Mitarbeit!";
+		Collection<Parent> list = parentService.getAllParents();
+		Iterator<Parent> iter = list.iterator();
+		while (iter.hasNext()) {
+			double costPerMonth = 0.0;
+			Parent p = iter.next();
+			for (Child c : p.getChildren()) {
+				costPerMonth += reportController.getLunchCostByChildMonthly(c);
+			}
+			mailService.sendEmail(p.getEmail(), "Essensabrechnung",
+					"Guten Tag " + p.getFirstName() + " " + p.getLastName()
+							+ "!\n\nWir senden Ihnen die monatliche Abrechnung für die konsumierten Mahlzeiten.:\n\n"
+							+ "Offener Betrag:  " + costPerMonth + " zahlbar binnen 14 Tagen."
+							+ "\n\n" + footer);
+		}
+	}*/
 }
