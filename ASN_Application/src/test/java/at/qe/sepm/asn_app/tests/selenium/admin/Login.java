3005108359f2d71ae8352b3c39b279e41dfd28d8
Bernd Menia <bernd.menia@student.uibk.ac.at>
2017-06-19 15:13:54 +0200

    - Created /selenium/Login - Changed name of selenium2/ to selenium/ - selenium/LoginLogout now uses selenium/Login

---
+++ ASN_Application/src/test/java/at/qe/sepm/asn_app/tests/selenium/admin/Login.java
@@ -0,0 +1,18 @@
+package at.qe.sepm.asn_app.tests.selenium.admin;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class Login {
+
+    public void login(WebDriver driver, String baseUrl) throws InterruptedException {
+        driver.get(baseUrl + "/login.xhtml");
+        driver.findElement(By.id("username")).clear();
+        driver.findElement(By.id("username")).sendKeys("admin");
+        driver.findElement(By.id("password")).clear();
+        driver.findElement(By.id("password")).sendKeys("passwd");
+        driver.findElement(By.cssSelector("form.login-form > button.login-button")).click();
+        Thread.sleep(1000);
+    }
+
+}
---
+++ ASN_Application/src/test/java/at/qe/sepm/asn_app/tests/selenium/admin/LoginLogout.java
@@ -0,0 +1,77 @@
+package at.qe.sepm.asn_app.tests.selenium.admin;
+
+import java.util.concurrent.TimeUnit;
+
+import at.qe.sepm.asn_app.tests.selenium.InitializeSelenium;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+import org.openqa.selenium.*;
+
+
+public class LoginLogout {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    driver = InitializeSelenium.initialize();
+    baseUrl = "http://192.168.33.10:8080/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testLoginLogout() throws Exception {
+      Login login = new Login();
+      login.login(driver, baseUrl);
+      driver.findElement(By.cssSelector("a.sidebar-toggle")).click();
+      Thread.sleep(1000);
+      driver.findElement(By.cssSelector("a.sidebar-toggle")).click();
+      Thread.sleep(1000);
+      driver.findElement(By.xpath("//div[@id='header']/div/header/nav/div/ul/li[2]/a/i")).click();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+}
+  }
+}
