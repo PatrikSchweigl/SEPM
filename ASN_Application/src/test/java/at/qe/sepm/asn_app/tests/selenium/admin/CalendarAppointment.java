e768a9fa2463b789e5a7f269b7aa000f48df3bd3
Bernd Menia <bernd.menia@student.uibk.ac.at>
2017-06-20 11:20:39 +0200

    Refactored code in selenium/admin/CalendarAppointment.

--- ASN_Application/src/test/java/at/qe/sepm/asn_app/tests/selenium/admin/CalendarAppointment.java
+++ ASN_Application/src/test/java/at/qe/sepm/asn_app/tests/selenium/admin/CalendarAppointment.java
@@ -1,20 +1,15 @@
 package at.qe.sepm.asn_app.tests.selenium.admin;
 
-import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
 
 import at.qe.sepm.asn_app.tests.selenium.InitializeSelenium;
 import org.junit.*;
 import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
 import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
 
 public class CalendarAppointment {
   private WebDriver driver;
   private String baseUrl;
-  private boolean acceptNextAlert = true;
   private StringBuffer verificationErrors = new StringBuffer();
 
   @Before
@@ -117,39 +112,6 @@ public class CalendarAppointment {
     String verificationErrorString = verificationErrors.toString();
     if (!"".equals(verificationErrorString)) {
       fail(verificationErrorString);
-    }
-  }
-
-  private boolean isElementPresent(By by) {
-    try {
-      driver.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
-
-  private boolean isAlertPresent() {
-    try {
-      driver.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
-
-  private String closeAlertAndGetItsText() {
-    try {
-      Alert alert = driver.switchTo().alert();
-      String alertText = alert.getText();
-      if (acceptNextAlert) {
-        alert.accept();
-      } else {
-        alert.dismiss();
-      }
-      return alertText;
-    } finally {
-      acceptNextAlert = true;
     }
   }
 }
