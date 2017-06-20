cae448d153d621caeab2e5052f45962c3c2b1c25
Bernd Menia <bernd.menia@student.uibk.ac.at>
2017-06-20 19:44:29 +0200

    Created selenium/parent/ParentTestSuite

---
+++ ASN_Application/src/test/java/at/qe/sepm/asn_app/tests/selenium/parent/ParentTestSuite.java
@@ -0,0 +1,17 @@
+package at.qe.sepm.asn_app.tests.selenium.parent;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Created by Bernd Menia <bernd.menia@student.uibk.ac.at>
+ * on 20.06.17 19:15 CEST.
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        CalendarLunchFastSignUp.class, 
+        CalendarSwitchViewsParent.class
+})
+
+public class ParentTestSuite {
+}
