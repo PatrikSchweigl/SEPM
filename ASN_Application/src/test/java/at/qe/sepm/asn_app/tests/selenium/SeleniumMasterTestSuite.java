9150a68570a423986899dbdecec5ab36783498a2
Bernd Menia <bernd.menia@student.uibk.ac.at>
2017-06-20 19:46:26 +0200

    Created selenium/MasterTestSuite; It includes all selenium test suites. --> All selenium tests can be started from the MasterSuite.

---
+++ ASN_Application/src/test/java/at/qe/sepm/asn_app/tests/selenium/SeleniumMasterTestSuite.java
@@ -0,0 +1,19 @@
+package at.qe.sepm.asn_app.tests.selenium;
+
+import at.qe.sepm.asn_app.tests.selenium.admin.AdminTestSuite;
+import at.qe.sepm.asn_app.tests.selenium.parent.ParentTestSuite;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Created by Bernd Menia <bernd.menia@student.uibk.ac.at>
+ * on 20.06.17 12:09 CEST.
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        AdminTestSuite.class,
+        ParentTestSuite.class
+})
+
+public class SeleniumMasterTestSuite {
+}
