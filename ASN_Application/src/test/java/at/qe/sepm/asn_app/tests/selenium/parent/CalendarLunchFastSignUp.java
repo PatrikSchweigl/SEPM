d6a4817d28ec890c5821ac70951173bb89249e43
Bernd Menia <bernd.menia@student.uibk.ac.at>
2017-06-20 19:32:23 +0200

    selenium/parent/CalnedarLunchFaytSignUp: Deleted unused code and comments and changed the indents from 2 to 4 spaces.

--- ASN_Application/src/test/java/at/qe/sepm/asn_app/tests/selenium/parent/CalendarLunchFastSignUp.java
+++ ASN_Application/src/test/java/at/qe/sepm/asn_app/tests/selenium/parent/CalendarLunchFastSignUp.java
@@ -1,131 +1,96 @@
 package at.qe.sepm.asn_app.tests.selenium.parent;
 
-import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
 
 import at.qe.sepm.asn_app.tests.selenium.InitializeSelenium;
 import org.junit.*;
 import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
 import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
+
 
 public class CalendarLunchFastSignUp {
-  private WebDriver driver;
-  private String baseUrl;
-  private boolean acceptNextAlert = true;
-  private StringBuffer verificationErrors = new StringBuffer();
+    private WebDriver driver;
+    private String baseUrl;
+    private StringBuffer verificationErrors = new StringBuffer();
 
-  @Before
-  public void setUp() throws Exception {
-    driver = InitializeSelenium.initialize();
-    baseUrl = "http://192.168.33.10:8080/";
-    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
-
-  @Test
-  public void testCalendarSwitchViewsParent2() throws Exception {
-    driver.get(baseUrl + "/login.xhtml");
-    driver.findElement(By.id("username")).clear();
-    driver.findElement(By.id("username")).sendKeys("bernd");
-    driver.findElement(By.id("password")).clear();
-    driver.findElement(By.id("password")).sendKeys("passwd");
-    driver.findElement(By.cssSelector("form.login-form > button.login-button")).click();
-    Thread.sleep(1000);
-    driver.findElement(By.xpath("//div[@id='dialogForm:dialogTest']/div/a/span")).click();
-    Thread.sleep(1000);
-    driver.findElement(By.id("j_idt118:j_idt120")).click();
-    Thread.sleep(1000);
-    driver.findElement(By.xpath("//div[@id='sheduleForm:massBox']/div[3]/table/tbody/tr/td[2]")).click();
-    Thread.sleep(1000);
-    driver.findElement(By.xpath("//div[@id='sheduleForm:massBox']/div[3]/table/tbody/tr[2]/td[2]")).click();
-    Thread.sleep(1000);
-    driver.findElement(By.xpath("//div[@id='sheduleForm:massBox']/div[3]/table/tbody/tr/td[2]")).click();
-    Thread.sleep(1000);
-    //driver.findElement(By.id("sheduleForm:massCheck:0_clone")).click();
-    Thread.sleep(1000);
-    driver.findElement(By.xpath("//div[@id='sheduleForm:opt11']/div[2]/span")).click();
-    Thread.sleep(1000);
-    driver.findElement(By.xpath("//div[@id='sheduleForm:opt11']/div[2]/span")).click();
-    //Thread.sleep(1000);
-    //driver.findElement(By.id("sheduleForm:massCheck:2_clone")).click();
-    Thread.sleep(1000);
-    driver.findElement(By.xpath("//div[@id='sheduleForm:opt21']/div[2]/span")).click();
-    Thread.sleep(1000);
-    driver.findElement(By.xpath("//div[@id='sheduleForm:opt21']/div[2]/span")).click();
-    Thread.sleep(1000);
-    //driver.findElement(By.id("sheduleForm:massCheck:4_clone")).click();
-    //Thread.sleep(1000);
-    driver.findElement(By.xpath("//div[@id='sheduleForm:opt31']/div[2]/span")).click();
-    Thread.sleep(1000);
-    driver.findElement(By.xpath("//div[@id='sheduleForm:opt41']/div[2]/span")).click();
-    Thread.sleep(1000);
-    driver.findElement(By.xpath("//div[@id='sheduleForm:opt51']/div[2]/span")).click();
-    Thread.sleep(1000);
-
-
-    driver.findElement(By.xpath("//div[@id='sheduleForm:opt12']/div[2]/span")).click();
-    Thread.sleep(1000);
-    driver.findElement(By.xpath("//div[@id='sheduleForm:opt12']/div[2]/span")).click();
-    Thread.sleep(1000);
-    driver.findElement(By.xpath("//div[@id='sheduleForm:opt22']/div[2]/span")).click();
-    Thread.sleep(1000);
-    driver.findElement(By.xpath("//div[@id='sheduleForm:opt22']/div[2]/span")).click();
-    Thread.sleep(1000);
-    driver.findElement(By.xpath("//div[@id='sheduleForm:opt32']/div[2]/span")).click();
-    Thread.sleep(1000);
-    driver.findElement(By.xpath("//div[@id='sheduleForm:opt42']/div[2]/span")).click();
-    Thread.sleep(1000);
-    driver.findElement(By.xpath("//div[@id='sheduleForm:opt52']/div[2]/span")).click();
-    Thread.sleep(1000);
-
-    driver.findElement(By.id("sheduleForm:j_idt242")).click();
-    Thread.sleep(1000);
-    driver.findElement(By.xpath("//div[@id='dialogForm:dialogTest']/div/a/span")).click();
-    Thread.sleep(1000);
-    driver.findElement(By.xpath("//div[@id='header']/div/header/nav/div/ul/li[3]/a/i")).click();
-  }
-
-  @After
-  public void tearDown() throws Exception {
-    driver.quit();
-    String verificationErrorString = verificationErrors.toString();
-    if (!"".equals(verificationErrorString)) {
-      fail(verificationErrorString);
+    @Before
+    public void setUp() throws Exception {
+        driver = InitializeSelenium.initialize();
+        baseUrl = "http://192.168.33.10:8080/";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
-  }
 
-  private boolean isElementPresent(By by) {
-    try {
-      driver.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
+    @Test
+    public void testCalendarSwitchViewsParent2() throws Exception {
+        // Login with a parent
+        driver.get(baseUrl + "/login.xhtml");
+        driver.findElement(By.id("username")).clear();
+        driver.findElement(By.id("username")).sendKeys("bernd");
+        driver.findElement(By.id("password")).clear();
+        driver.findElement(By.id("password")).sendKeys("passwd");
+        driver.findElement(By.cssSelector("form.login-form > button.login-button")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//div[@id='dialogForm:dialogTest']/div/a/span")).click();
+        Thread.sleep(1000);
 
-  private boolean isAlertPresent() {
-    try {
-      driver.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
+        // Click on the fast sign-up button.
+        driver.findElement(By.id("j_idt118:j_idt120")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//div[@id='sheduleForm:massBox']/div[3]/table/tbody/tr/td[2]")).click();
+        Thread.sleep(1000);
 
-  private String closeAlertAndGetItsText() {
-    try {
-      Alert alert = driver.switchTo().alert();
-      String alertText = alert.getText();
-      if (acceptNextAlert) {
-        alert.accept();
-      } else {
-        alert.dismiss();
-      }
-      return alertText;
-    } finally {
-      acceptNextAlert = true;
+        // General sign-up.
+        // Click the first two checkboxes twice to show that the can be un-assigned.
+        driver.findElement(By.xpath("//div[@id='sheduleForm:opt11']/div[2]/span")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//div[@id='sheduleForm:opt11']/div[2]/span")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//div[@id='sheduleForm:opt21']/div[2]/span")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//div[@id='sheduleForm:opt21']/div[2]/span")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//div[@id='sheduleForm:opt31']/div[2]/span")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//div[@id='sheduleForm:opt41']/div[2]/span")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//div[@id='sheduleForm:opt51']/div[2]/span")).click();
+        Thread.sleep(1000);
+
+        // Do the same for the lunch.
+        driver.findElement(By.xpath("//div[@id='sheduleForm:opt12']/div[2]/span")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//div[@id='sheduleForm:opt12']/div[2]/span")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//div[@id='sheduleForm:opt22']/div[2]/span")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//div[@id='sheduleForm:opt22']/div[2]/span")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//div[@id='sheduleForm:opt32']/div[2]/span")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//div[@id='sheduleForm:opt42']/div[2]/span")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//div[@id='sheduleForm:opt52']/div[2]/span")).click();
+        Thread.sleep(1000);
+
+        // Save the changes made.
+        driver.findElement(By.id("sheduleForm:j_idt242")).click();
+        Thread.sleep(1000);
+
+        // Since the page refresehs and we are on the main page of the parent we need to
+        // close the password change modal again.
+        driver.findElement(By.xpath("//div[@id='dialogForm:dialogTest']/div/a/span")).click();
+        Thread.sleep(1000);
+
+        // Logout.
+        driver.findElement(By.xpath("//div[@id='header']/div/header/nav/div/ul/li[3]/a/i")).click();
     }
-  }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+    }
 }
