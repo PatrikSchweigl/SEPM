edbc6d44443fc9858c48a6f16b4cd10804577077
Bernd Menia <bernd.menia@student.uibk.ac.at>
2017-06-20 19:21:57 +0200

    Renamed selenium/parent/CalendarSwitchViewsParent to selenium/parent/CalendarLunchFastSignUp.

--- ASN_Application/src/test/java/at/qe/sepm/asn_app/tests/selenium/parent/CalendarLunchFastSignUp.java
+++ ASN_Application/src/test/java/at/qe/sepm/asn_app/tests/selenium/parent/CalendarLunchFastSignUp.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class CalendarSwitchViewsParent2 {
+public class CalendarLunchFastSignUp {
   private WebDriver driver;
   private String baseUrl;
   private boolean acceptNextAlert = true;
