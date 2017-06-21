73b6971e4fbd159c7c947f53394bdc7d9dab4fe6
Bernd Menia <bernd.menia@student.uibk.ac.at>
2017-06-21 20:13:43 +0200

    ControllerTestSuite: Replaced the dangling class JavaDoc.

--- ASN_Application/src/test/java/at/qe/sepm/asn_app/tests/controller/ControllerTestSuite.java
+++ ASN_Application/src/test/java/at/qe/sepm/asn_app/tests/controller/ControllerTestSuite.java
@@ -1,19 +1,18 @@
 package at.qe.sepm.asn_app.tests.controller;
 
-/**
- * Created by Bernd Menia <bernd.menia@student.uibk.ac.at>
- * on 12.06.17 10:57 CEST.
- */
-
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+
+/**
+ * Created by Bernd Menia <bernd.menia@student.uibk.ac.at>
+ * on 12.06.17 10:57 CEST.
+ */
 @RunWith(Suite.class)
 @SuiteClasses({
-        EnumControllerTest.class,
+        EnumControllerTest.class
 })
 
 public class ControllerTestSuite {
-
 }
